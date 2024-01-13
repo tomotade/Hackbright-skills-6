2c51b56bf336e5187d528476e5c2d500099c292f
tomotade <49998516+tomotade@users.noreply.github.com>
2024-01-12 21:06:04 -0600

    Hackbright-skills-6

---
+++ __tests__/shuffle.test.js
@@ -0,0 +1,12 @@
+const shuffle = require("../src/shuffle");
+
+describe("shuffle should...", () => {
+  // CODE HERE
+  test("return a number if i won the fight game", () => {
+    expect(math.abs(1)).toBe(1);
+  });
+
+  test("return a positive if i loss the fight game", () => {
+    expect(math.abs(-1)).toBe(1);
+  });
+});
